ac882ec0d26c173d9d7dc9e9e6ede2d454f63601
Nishant Nayar <nishant.nayar@hotmail.com>
2025-06-10 09:43:30 -0500

    Update alpaca_websocket.py

--- src/data/alpaca_websocket.py
+++ src/data/alpaca_websocket.py
@@ -52,8 +52,8 @@ async def websocket_connection():
         api_key_block = await Secret.load("alpaca-api-key")
         secret_key_block = await Secret.load("alpaca-secret-key")
         
-        API_KEY = await api_key_block.get()
-        API_SECRET = await secret_key_block.get()
+        API_KEY = api_key_block.get()
+        API_SECRET = secret_key_block.get()
         
         logger.info("Successfully loaded API credentials")
     except Exception as e:
